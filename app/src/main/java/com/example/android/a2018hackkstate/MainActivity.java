b435d23914bb28f673f7525c8fd1681003c4a33c
Alex Masson <amasson99@gmail.com>
2018-10-19 20:50:36 -0500

    Initial Commit

---
+++ app/src/main/java/com/example/android/a2018hackkstate/MainActivity.java
@@ -0,0 +1,13 @@
+package com.example.android.a2018hackkstate;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+
+public class MainActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+    }
+}
